8545300c0591b13cd59361eaa37965faa0077805
Xander Dunn <xander@xander.ai>
2023-09-21 12:09:21 +0200

    Enable cargo test in the test.sh

--- test.sh
+++ test.sh
@@ -7,8 +7,7 @@ eval $(minikube -p minikube docker-env)
 docker build -t kube-state-rs .
 kubectl apply -f kube-state-rs.yaml
 # kubectl describe pod
-#kubectl delete pod 
 kubectl get pods --no-headers -o custom-columns=":metadata.name" | grep 'kube-state-rs' | xargs -I {} kubectl delete pod {}
 kubectl get pods -l app=kube-state-rs
 # kubectl logs -f
-# cargo test
+cargo test
